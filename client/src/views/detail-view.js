01d90d9fd8a40dd07dc1791709b681a6aff1cfec
Elijah Boston <e.b.boston@gmail.com>
2019-10-13 12:55:20 -0400

    Placeholder data and linking between views

--- client/src/views/detail-view.js
+++ client/src/views/detail-view.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from '@reach/router';
 
 const DetailView = ({ slug }) =>
   <main>
     <h1>Detail View</h1>
+    <p>{slug}</p>
+    <Link to='/'>Back</Link>
   </main>;
+
+DetailView.propTypes = {
+  slug: PropTypes.string
+}
+
+DetailView.defaultProps = {
+  slug: ''
+}
 
 export default DetailView;
