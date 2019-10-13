c07a7c258e30737021551fca06b1b43876f35079
Elijah Boston <e.b.boston@gmail.com>
2019-10-13 18:56:51 -0400

    Move global styles to a static CSS file

--- client/src/app.js
+++ client/src/app.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import { Router } from "@reach/router";
 import { MainView, DetailView } from './views'
+
 const App = () =>
   <div>
     <Router>
       <MainView path="/" />
       <DetailView path="/:slug" />
     </Router>
-    <style jsx global>{`
-      body {
-        padding: 0;
-        margin: 0;
-      }
-    `}</style>
   </div>;
 
     
