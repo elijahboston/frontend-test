3b5867f2f741eabadfc4bd9352daa53282233395
Elijah Boston <e.b.boston@gmail.com>
2019-10-13 19:04:48 -0400

    Import global styles

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,3 +1,6 @@
 import { configure } from '@storybook/react';
 
+import '../static/global.css';
+import '../static/icons.css';
+
 configure(require.context('../src', true, /\.stories\.js$/), module);
