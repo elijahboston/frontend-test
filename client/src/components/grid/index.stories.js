340b9a250abe217461cd6ca3ac41f4c59f48c757
Elijah Boston <e.b.boston@gmail.com>
2019-10-14 01:30:31 -0400

    Cleanup stories and use helper function

--- client/src/components/grid/index.stories.js
+++ client/src/components/grid/index.stories.js
@@ -1,38 +1,8 @@
 import React from 'react';
 import Grid from '.';
-import GridItem from '../grid-item';
-
-const gridItemData = {
-  "name": "Yardbird Southern Table & Bar",
-  "alias": "yardbird-southern-table-and-bar-las-vegas",
-  "photos": [
-    "https://picsum.photos/300/300"
-  ],
-  "rating": 4.5,
-  "price": "$$",
-  "categories": [
-    {
-      "title": "Southern",
-      "alias": "southern"
-    },
-    {
-      "title": "American (New)",
-      "alias": "newamerican"
-    }
-  ],
-  "is_closed": false // eslint-disable-line camelcase
-};
+import { buildGrid } from '../../test-helpers/_helpers';
 
 export default { title: 'Grid' };
-
-const buildGrid = (n) => {
-  const items = [];
-  while (n > items.length) {
-    items.push(<GridItem id={items.length} { ...gridItemData } isClosed={gridItemData.is_closed} />)
-  }
-
-  return items
-}
 
 export const withOneItem = () =>
   <Grid>
