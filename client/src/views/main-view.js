35a6c2507da9174043e9574cde120c9bc3506159
Elijah Boston <e.b.boston@gmail.com>
2019-10-14 23:21:47 -0400

    Update import

--- client/src/views/main-view.js
+++ client/src/views/main-view.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Header, Filter, Grid, GridItem } from '../components';
-import { mockTitle, mockDescription } from '../test-helpers/_mock-data';
+import { mockTitle, mockDescription } from '../storybook-helpers/_mock-data';
 
 const MainView = ({ items }) =>
   <main>
