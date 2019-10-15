26c920591b18a0a3805a5660e8116ff4775781d4
Elijah Boston <e.b.boston@gmail.com>
2019-10-15 12:26:19 -0400

    Add grid header

--- client/src/views/main-view.js
+++ client/src/views/main-view.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Header, Filter, Grid, GridItem } from '../components';
+import { Header, Filter, Grid, GridItem, CenterContent } from '../components';
 import { mockTitle, mockDescription } from '../storybook-helpers/_mock-data';
 
 const MainView = ({ items }) =>
   <main>
     <Header title={mockTitle} description={mockDescription} />
     <Filter />
-    
+    <CenterContent>
+      <h2>All Restaurants</h2>
+    </CenterContent>
     <Grid>
       {items.map(item => <GridItem key={item.id} { ...item } isOpen={item.is_open} />)}
     </Grid>
