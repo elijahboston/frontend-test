8a45d86df8069e7b5acfa94f5ee656dc5d9dcc5f
Elijah Boston <e.b.boston@gmail.com>
2019-10-14 22:50:49 -0400

    Move test helpers

--- client/src/test-helpers/_setup-browser-env.js
+++
@@ -1,5 +0,0 @@
-import browserEnv from 'browser-env';
-
-// browserEnv uses jsdom to create a mock browser environment.
-// Here we specify what browser components we want to mock globally
-browserEnv(['window', 'document', 'navigator']);
