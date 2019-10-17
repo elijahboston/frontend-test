5b6a6f46f68f5d666ffb3065ed8316a7238bd5f8
Elijah Boston <e.b.boston@gmail.com>
2019-10-17 00:29:17 -0400

    Minor naming update

--- client/src/components/svg/dot.js
+++ client/src/components/svg/dot.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const Dot = ({ width='.8rem', height='.8rem', fill }) =>
   <svg width={width} height={height} viewBox="0 0 8 8">
-    <circle cx="4" cy="9" r="4" transform="translate(0 -5)" fill={fill} fill-rule="evenodd"></circle>
+    <circle cx="4" cy="9" r="4" transform="translate(0 -5)" fill={fill} fillRule="evenodd"></circle>
   </svg>;
 
 Dot.propTypes = {
