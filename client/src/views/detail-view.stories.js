ede6de74c440268cf9db9203aab664ce566b46e3
Elijah Boston <e.b.boston@gmail.com>
2019-10-16 23:19:04 -0400

    Delete stories for views

--- client/src/views/detail-view.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import DetailView from './detail-view';
-
-const stories = storiesOf('Detail View', module);
-
-stories.add('Default', () => (
-  <DetailView slug='test-slug' />
-));
