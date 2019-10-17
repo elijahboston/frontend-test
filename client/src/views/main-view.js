0b4e2f57596fb6c10d2de384d1d7c88df96595a6
Elijah Boston <e.b.boston@gmail.com>
2019-10-17 14:39:46 -0400

    Use data views

--- client/src/views/main-view.js
+++ client/src/views/main-view.js
@@ -1,21 +1,10 @@
 import React, { useContext } from 'react';
-import PropTypes from 'prop-types';
-import { Header, Filter, Grid, GridItem, CenterContent } from '../components';
-import { LoadMoreButton } from '../components/button';
-import { useQuery } from '@apollo/react-hooks';
-import { MAIN_QUERY } from '../graphql';
+import { Header, CenterContent } from '../components';
 import { AppContext } from '../contexts';
+import SearchView from './search-view';
+import FilterView from './filter-view';
 
 import { mockTitle, mockDescription } from '../storybook-helpers/_mock-data';
-
-// Hardcode these since they don't change
-const filterOptions = [
-  { title: 'All', alias: 0 },
-  { title: '$', alias: 1 },
-  { title: '$$', alias: 2 },
-  { title: '$$$', alias: 3 },
-  { title: '$$$$', alias: 4 },
-];
 
 const MainView = () => {
   const {
@@ -24,46 +13,14 @@ const MainView = () => {
     categoriesSelected
   } = useContext(AppContext);
 
-  const queryParams = {
-    categories: categoriesSelected.join(','),
-    price: pricesSelected.join(','),
-    openNow: openNow
-  };
-
-  const { loading, error, data } = useQuery(
-    MAIN_QUERY,
-    { variables: { ...queryParams } }
-  );
-  
-  if (loading) return (<div>Loading...</div>);
-
-  if (error) return (<div>{error}</div>);
-
-  const {
-    categories: {
-      category: categoryOptions
-    },
-    search: {
-      business: items
-    }
-  } = data;
-
   return (
     <main>
       <Header title={mockTitle} description={mockDescription} />
-      <Filter priceOptions={filterOptions} categoryOptions={categoryOptions} />
+      <FilterView />
       <CenterContent>
         <h2>All Restaurants</h2>
       </CenterContent>
-      <Grid rows={items.length > 0 ? 2 : 0}>
-        {items.map(item => {
-          const category = item.categories[0].title;
-          return (<GridItem key={item.id} { ...item } category={category} isOpen={!item.is_closed} />);
-        })}
-      </Grid>
-      <div className='load-more'>
-        <LoadMoreButton>Load More</LoadMoreButton>
-      </div>
+      <SearchView categories={categoriesSelected} price={pricesSelected} isOpen={openNow} />
       <style jsx>{`
         .load-more {
           display: flex;
