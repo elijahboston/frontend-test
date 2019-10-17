523cc798fb0df33a3e7e9f807265466037f59109
Elijah Boston <e.b.boston@gmail.com>
2019-10-17 14:33:48 -0400

    Wrap all storybook components with ThemeProvider

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,5 +1,8 @@
-import { configure } from '@storybook/react';
-
+import React from 'react';
+import { configure, addDecorator } from '@storybook/react';
 import '../src/storybook-helpers/global.css';
+import { AppProvider, ThemeProvider } from '../src/contexts';
+
+addDecorator(storyFn => <ThemeProvider>{storyFn()}</ThemeProvider>);
 
 configure(require.context('../src/components', true, /\.stories\.js$/), module);
