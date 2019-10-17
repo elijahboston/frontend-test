2b9cd63e0a405116437dedc143986349cc63dce6
Elijah Boston <e.b.boston@gmail.com>
2019-10-17 14:40:01 -0400

    Export queries

--- client/src/graphql/index.js
+++ client/src/graphql/index.js
@@ -1,2 +1,4 @@
 export {default as DETAIL_QUERY} from './detail-query';
 export {default as MAIN_QUERY} from './main-query';
+export {default as SEARCH_QUERY} from './search-query';
+export {default as CATEGORIES_QUERY} from './categories-query';
