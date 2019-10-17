c0f6834ea785da4e4a5d5a84ad8374028650c0bb
Elijah Boston <e.b.boston@gmail.com>
2019-10-17 00:29:55 -0400

    Hook up filter to main page query

--- client/src/graphql/main-query.js
+++ client/src/graphql/main-query.js
@@ -1,26 +1,28 @@
 import gql from 'graphql-tag';
 
-const MAIN_QUERY = gql`{
-  search(location:"san francisco" limit:8) {
-    business {
-      id
-      alias
-      name
-      price
-      categories {
-        title
+const MAIN_QUERY = gql`
+  query MainView($price: String $categories: String $openNow: Boolean) {
+    search(location:"las vegas" limit:8 price:$price categories:$categories open_now:$openNow) {
+      business {
+        id
         alias
+        name
+        price
+        categories {
+          title
+          alias
+        }
+        is_closed
+        photos
       }
-      is_closed
-      photos
+    }
+    categories {
+        category {
+          title
+          alias
+        }
     }
   }
-  categories {
-      category {
-        title
-        alias
-      }
-  }
-}`;
+`;
 
 export default MAIN_QUERY;
--- client/src/views/main-view.js
+++ client/src/views/main-view.js
@@ -1,36 +1,65 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Header, Filter, Grid, GridItem, CenterContent } from '../components';
 import { LoadMoreButton } from '../components/button';
 import { useQuery } from '@apollo/react-hooks';
 import { MAIN_QUERY } from '../graphql';
+import { AppContext } from '../contexts';
 
 import { mockTitle, mockDescription } from '../storybook-helpers/_mock-data';
 
+// Hardcode these since they don't change
 const filterOptions = [
-  { title: 'All', alias: 'all' },
-  { title: '$', alias: '$' },
-  { title: '$$', alias: '$$' },
-  { title: '$$$', alias: '$$$' },
-  { title: '$$$$', alias: '$$$$' },
+  { title: 'All', alias: 0 },
+  { title: '$', alias: 1 },
+  { title: '$$', alias: 2 },
+  { title: '$$$', alias: 3 },
+  { title: '$$$$', alias: 4 },
 ];
 
 const MainView = () => {
-  const { loading, error, data } = useQuery(MAIN_QUERY);
+  const {
+    openNow,
+    pricesSelected,
+    categoriesSelected
+  } = useContext(AppContext);
+
+  const queryParams = {
+    categories: categoriesSelected.join(','),
+    price: pricesSelected.join(','),
+    openNow: openNow
+  };
+
+  const { loading, error, data } = useQuery(
+    MAIN_QUERY,
+    { variables: { ...queryParams } }
+  );
   
   if (loading) return (<div>Loading...</div>);
 
   if (error) return (<div>{error}</div>);
 
+  const {
+    categories: {
+      category: categoryOptions
+    },
+    search: {
+      business: items
+    }
+  } = data;
+
   return (
     <main>
       <Header title={mockTitle} description={mockDescription} />
-      <Filter priceOptions={filterOptions} categoryOptions={data.categories.category} />
+      <Filter priceOptions={filterOptions} categoryOptions={categoryOptions} />
       <CenterContent>
         <h2>All Restaurants</h2>
       </CenterContent>
       <Grid rows={items.length > 0 ? 2 : 0}>
-        {data.search.business.map(item => <GridItem key={item.id} { ...item } isOpen={item.is_open} />)}
+        {items.map(item => {
+          const category = item.categories[0].title;
+          return (<GridItem key={item.id} { ...item } category={category} isOpen={!item.is_closed} />);
+        })}
       </Grid>
       <div className='load-more'>
         <LoadMoreButton>Load More</LoadMoreButton>
