e447b5cc7677a7c2c14c4697043c1bdb707dbef0
Elijah Boston <e.b.boston@gmail.com>
2019-10-18 04:47:12 -0400

    Use global.css

--- client/src/app.js
+++ client/src/app.js
@@ -4,6 +4,7 @@ import { HomePage, DetailPage } from './pages';
 import { ThemeProvider, AppProvider } from './contexts';
 import apolloClient from './helpers/apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import './css/global.css';
 
 const App = () => {
   return (
@@ -18,17 +19,6 @@ const App = () => {
           </ThemeProvider>
         </AppProvider>
       </ApolloProvider>
-      <style jsx global>{`
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: 'Helvetica Neue', san-serif;
-        }
-        
-        h1, h2 {
-          font-weight: lighter;
-        }
-      `}</style>
     </div>
   );
 }
